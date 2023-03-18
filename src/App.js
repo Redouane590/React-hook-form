ebe6cbeacc1e5f3bee3ee212182818d11699f324
Redouane Elmahi <redouaneelmahi@gmail.com>
2023-03-18 18:48:16 +0100

    a

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div
       className="d-flex justify-content-center align-items-center"
-      style={{ backgroundColor: '#fefefe', height: '100vh', width: '100%' }}
+      style={{ backgroundColor: '#aaaaaa', height: '100vh', width: '100%' }}
     >
       <h1>Hello World</h1>
     </div>
